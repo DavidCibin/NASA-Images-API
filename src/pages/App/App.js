acdd17f51eeac9c2a024b34c710761ac7a5fe38d
davidisurf <davidisurf@hotmail.com>
2021-04-14 00:31:01 -0400

    Add daily pic view component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,22 +3,27 @@ import { Route } from 'react-router-dom'
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar'
 import NasaMars from '../NasaMars/NasaMars'
+import NasaDaily from '../NasaDaily/NasaDaily'
 
 
 class App extends Component {
     state = {
         navItems: [
-            { url: "/nasa", name: "Nasa Mars Rover Photos" },
+            { url: "/nasa-daily", name: " Astronomy Picture of the Day" },
+            { url: "/nasa-mars", name: "Nasa Mars Rover Photos" },
         ]
     }
     render() {
         return (
             <>
                 <NavBar navItems={this.state.navItems} />
-                <Route exact path='/nasa' render={() => <NasaMars />} />
+                <Route exact path='/nasa-mars' render={() => <NasaMars />} />
+                <Route exact path='/nasa-daily' render={() => <NasaDaily />} />
+                
+                
                 {/* <Route exact path='/starship' render={( { location } ) => <StarshipDetails location={location} />} /> */}
-
             </>
+
         )
     }
 
---
+++ src/pages/NasaDaily/NasaDaily.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NasaDaily extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <h1>daily pics</h1>
+      </div>
+     );
+  }
+}
+ 
+export default NasaDaily;
