73164d6b282242639e4cde84194803d42d9af1f6
davidisurf <davidisurf@hotmail.com>
2021-04-15 00:42:03 -0400

    CSS Style and add logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 const NavBar = (props) => {
   return(
     <header className="App-header">
+      <img
+        src='https://api.nasa.gov/assets/img/favicons/favicon-192.png'
+        style={{ width: "100px", height: "100px" }}
+        className="App-logo"
+        alt="logo"
+      />
       {props.navItems.map((navItem, idx) =>
         <a key={idx} href={navItem.url}>{navItem.name}</a>
       )}
