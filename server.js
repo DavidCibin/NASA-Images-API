f84626f41ddc0dc3cc983c6d6b9ca7d64da41887
David Cibin <ddcibin@gmail.com>
2023-10-05 23:20:46 -0400

    update server

--- server.js
+++ server.js
@@ -10,8 +10,22 @@ require('dotenv').config();
 const apiRouter = require('./routes/nasa');
 const cors = require('cors');
 
+app.use((req, res, next) => {
+    console.log(req.headers, "HERE HERE HERE HERE HERE HERE ");
+    next();
+});
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // Replace this with your frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+};
+
+
+
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(cors());
+// Apply CORS middleware with specified options
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use('/api', apiRouter);
@@ -22,8 +36,5 @@ app.listen(port, ()=> {
     console.log(`Express is listening on port ${port}.`)
 });
 
-app.use((req, res, next) => {
-    console.log(req.headers, "HERE HERE HERE HERE HERE HERE ");
-    next();
-  });
+
   
