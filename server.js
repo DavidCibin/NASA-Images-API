ffe0eb1e1a11b8f735a09916aa8c834c8d0363a1
David Cibin <ddcibin@gmail.com>
2023-10-16 09:35:07 -0400

    update port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app = express();
+const bodyParser = require("body-parser")
+const app = express(bodyParser.json());
 const path = require('path');
 const logger = require('morgan');
 
